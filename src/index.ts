289b4f73c6290ce987bec7e54af969119db5eac3
Logan McAnsh <logan@mcan.sh>
2023-06-23 00:04:09 -0400

    chore: update implementation
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,44 @@
+import * as React from "react";
 import type { Metric } from "web-vitals";
-import { onCLS, onFCP, onFID, onLCP, onINP, onTTFB } from "web-vitals";
-import { useLocation } from "react-router-dom";
+import type { Params } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
+import { onCLS, onFCP, onFID, onLCP, onTTFB, onINP } from "web-vitals";
 
-let isRegistered = false;
-
-function onError(error: unknown) {
-  console.error(`[@mcansh/remix-web-vitals]`, error);
-}
-
-function onDebug(label: string, ...args: any[]) {
-  console.debug(label, ...args);
-}
+let vitalsUrl = "https://vitals.vercel-analytics.com/v1/vitals";
 
 type Options = {
-  debug?: boolean;
   analyticsId: string;
+  debug?: boolean;
 };
 
 type FullOptions = Options & {
-  fullPath: string;
-  href: string;
+  params: Readonly<Params<string>>;
+  path: string;
 };
 
 function sendToAnalytics(metric: Metric, options: FullOptions) {
+  let page = Object.entries(options.params).reduce((acc, [key, value]) => {
+    return value ? acc.replace(value, `[${key}]`) : ``;
+  }, options.path);
+
   let body = {
-    dsn: options.analyticsId,
-    id: metric.id,
-    page: options.fullPath,
-    href: options.href,
-    event_name: metric.name,
-    value: metric.value.toString(),
-    speed: getConnectionSpeed(),
+    dsn: options.analyticsId, // qPgJqYH9LQX5o31Ormk8iWhCxZO
+    id: metric.id, // v2-1653884975443-1839479248192
+    page, // /blog/[slug]
+    href: location.href, // https://{my-example-app-name-here}/blog/my-test
+    event_name: metric.name, // TTFB
+    value: metric.value.toString(), // 60.20000000298023
+    speed: getConnectionSpeed(), // 4g
   };
 
   if (options.debug) {
-    onDebug(metric.name, JSON.stringify(body, null, 2));
+    onDebug(metric.name, body);
   }
 
   let blob = new Blob([new URLSearchParams(body).toString()], {
-    // This content type is necessary for `sendBeacon`:
+    // This content type is necessary for `sendBeacon`
     type: "application/x-www-form-urlencoded",
   });
-
-  let vitalsUrl = "https://vitals.vercel-analytics.com/v1/vitals";
-
   if (navigator.sendBeacon) {
     navigator.sendBeacon(vitalsUrl, blob);
   } else {
@@ -52,42 +47,57 @@ function sendToAnalytics(metric: Metric, options: FullOptions) {
       body: blob,
       credentials: "omit",
       keepalive: true,
-    }).catch(onError);
+    });
   }
 }
 
-export function getConnectionSpeed(): string {
-  return (
-    (typeof navigator !== "undefined" &&
-      // @ts-expect-error
-      navigator.connection &&
-      // @ts-expect-error
-      navigator.connection.effectiveType) ||
-    ""
-  );
-}
-
-export async function useWebVitals(options: Options) {
+let isRegistered = false;
+export function WebVitals(options: Options) {
   let location = useLocation();
+  let params = useParams();
 
-  // only register listeners once
-  if (isRegistered) return;
-  isRegistered = true;
+  React.useEffect(() => {
+    (() => {
+      if (isRegistered) {
+        return;
+      }
 
-  let fullOptions: FullOptions = {
-    ...options,
-    fullPath: location.pathname + location.search + location.hash,
-    href: location.pathname,
-  };
+      let fullOptions = {
+        ...options,
+        params,
+        path: location.pathname,
+      } as const;
 
-  try {
-    onFID((metric) => sendToAnalytics(metric, fullOptions));
-    onTTFB((metric) => sendToAnalytics(metric, fullOptions));
-    onLCP((metric) => sendToAnalytics(metric, fullOptions));
-    onCLS((metric) => sendToAnalytics(metric, fullOptions));
-    onFCP((metric) => sendToAnalytics(metric, fullOptions));
-    onINP((metric) => sendToAnalytics(metric, fullOptions));
-  } catch (error) {
-    onError(error);
-  }
+      try {
+        onFID((metric) => sendToAnalytics(metric, fullOptions));
+        onTTFB((metric) => sendToAnalytics(metric, fullOptions));
+        onLCP((metric) => sendToAnalytics(metric, fullOptions));
+        onCLS((metric) => sendToAnalytics(metric, fullOptions));
+        onFCP((metric) => sendToAnalytics(metric, fullOptions));
+        onINP((metric) => sendToAnalytics(metric, fullOptions));
+      } catch (error) {
+        onError(error);
+      }
+    })();
+  }, [location.hash, location.pathname, location.search, options, params]);
+
+  return null;
+}
+
+function getConnectionSpeed(): string {
+  // @ts-expect-error
+  return "connection" in navigator &&
+    navigator["connection"] &&
+    // @ts-expect-error
+    "effectiveType" in navigator["connection"]
+    ? navigator["connection"]["effectiveType"]
+    : "";
+}
+
+function onError(error: unknown) {
+  console.error(`[@mcansh/remix-web-vitals]`, error);
+}
+
+function onDebug(label: string, ...args: any[]) {
+  console.debug(label, ...args);
 }
